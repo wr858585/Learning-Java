118a3fab55101b4c9986933b6d1fb2e6de590f67
oono <andychao3210@gmail.com>
2020-04-27 10:12:15 +1000

    2020.4.27 commits1

--- day05/src/com/oono/java/ArrayTest2.java
+++ day05/src/com/oono/java/ArrayTest2.java
@@ -1,8 +1,22 @@
 package com.oono.java;
+/*
+ * 二、二维数组
+ * 
+ * 1.理解
+ * arrya1的ele是array_1a,array_1b etc
+ * 从底层看，其实并没有二维数组的概念。
+ * array1的1st ele指向array_1a整体，2nd ele指向array_1b整体
+ * 
+ * 2.需要掌握的点
+ *   ① 一维数组的声明和初始化
+ *   ② 如何call数组的指定位置的ele
+ *   ③ 如何获取数组的length
+ *   ④ 如何遍历traverse数组
+ *   ⑤ 数组ele的默认初始化值,see ArrayTest1.java
+ *   ⑥ 数组的内存解析, see ArrayTest1.java
+ *   
+ */
 
 public class ArrayTest2 {
-//test
-	//test
-	//test
-	//test
+	
 }
