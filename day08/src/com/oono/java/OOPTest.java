fca052add8196a2c38ff3748ac7785906967fd91
oono <andychao3210@gmail.com>
2020-05-03 21:40:36 +1000

    2020.5.3 commits

---
+++ day08/src/com/oono/java/OOPTest.java
@@ -0,0 +1,11 @@
+package com.oono.java;
+/*
+ * 一、JAVA面向对象OOP Object-Oriented Programming的三大学习主线（4-6章）
+ * 1.Java类及其成员：属性、方法、构造器；代码块、内部类
+ * 2.OOP的三大特征：封装性、继承性、多态性
+ * 3.其他关键字：this,super,static,final,abstract,interface,package,import
+ * 
+ */
+public class OOPTest {
+
+}
