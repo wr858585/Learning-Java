858fa934c8342848b20437dd3d4e69df278349f6
oono <andychao3210@gmail.com>
2020-05-18 21:15:58 +1000

    2020.5.18 commits

---
+++ day09/src/com/oono/exer1/PassObject.java
@@ -0,0 +1,38 @@
+package com.oono.exer1;
+
+/*
+ * 定义一个PassObject，在类中定义一个方法printAreas（）
+ * 该方法的定义如下：public void printAreas(Circle c, int time)
+ * 在printAreas方法中打印输出1到times之间的每个证书半径值，以及对应的面积
+ * 例如：times为5，则输出半径为1，2，3，4，5，以及对应的圆面积
+ * 
+ * 在main方法中call printAreas（）方法，调用完毕后输出当前半径值。
+ * 
+ */
+public class PassObject {
+
+	public static void main(String[] args) {
+		PassObject test = new PassObject();
+
+		Circle c = new Circle();
+
+		test.printAreas(c, 5);
+
+		System.out.println("now radius is " + c.radius);
+	}
+
+	public void printAreas(Circle c, int times) {
+
+		System.out.println("Radius\t\tArea");
+
+		int i = 1;
+		// 设置圆的半径
+		for (; i <= times; i++) {
+			c.radius = i;
+			System.out.println(c.radius + "\t\t" + c.findArea());
+		}
+
+		c.radius = i;
+
+	}
+}
---
+++ day09/src/com/oono/java2/RecursionTest.java
@@ -0,0 +1,29 @@
+package com.oono.java2;
+/*
+ * 递归方法的使用（了解）
+ * 1.递归recursion：一个方法体内call本身
+ * 2.方法递归包含了一种隐式的loop，它会重复执行某段代码，但这种重复执行无需loop控制
+ * 
+ * 
+ */
+public class RecursionTest {
+	
+	public static void main(String[] args) {
+		
+		//例1.计算1-100之间所有自然数的和
+		//方式一：
+//		int sum = 0;
+//		for(int i = 0; i <= 100; i++){
+//			sum += 1;
+//		}
+		//方式二：
+		
+	}
+	
+	public int getSum(int n){
+		
+		
+	}
+
+
+}
