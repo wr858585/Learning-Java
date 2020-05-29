36aceccc783996a36f180433010077ad6038c003
oono <andychao3210@gmail.com>
2020-05-29 18:19:40 +0800

    2020.5.29 commits

--- day10/src/com/oono/java1/OrderTest.java
+++ day10/src/com/oono/java1/OrderTest.java
@@ -1,22 +1,18 @@
-package com.oono.java;
-/*
- * 
- * 
- * 
- */
+package com.oono.java1;
+
+import com.oono.java.Order;
+
 public class OrderTest {
 	public static void main(String[] args) {
-		
 		Order order = new Order();
 		
 		//测试各类修饰符对order类中的属性和方法的限制程度
 		order.orderPublic = 1;
-		order.orderDefault = 2;
+//		order.orderDefault = 2;缺省的的在不同package中无法调用
 //		order.orderPrivate = 3;无法直接调用,private只能在同一个类的内部
 		
 		order.methodPublic();
-		order.methodDefault();
+//		order.methodDefault();缺省的在不同package中无法调用
 //		order.methodPrivate();也是提示not visible
-		
 	}
 }
