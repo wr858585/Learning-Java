d6247814869ab45520b9bd6bce6ffc2a3dcf7904
oono <andychao3210@gmail.com>
2020-06-10 21:02:23 +0800

    2020.6.10 commits

---
+++ myproject02/src/com/oono/p2/bean/Customer.java
@@ -0,0 +1,73 @@
+package com.oono.p2.bean;
+
+/**
+ * @Description Customer为实体对象，用来封装客户
+ * @author oono
+ *
+ */
+public class Customer {
+
+	// field
+	private String name;
+	private char gender;
+	private int age;
+	private String phone;
+	private String email;
+
+	// constructor
+	
+	public Customer() {
+	}
+
+	public Customer(String name, char gender, int age, String phone, String email) {
+		this.name = name;
+		this.gender = gender;
+		this.age = age;
+		this.phone = phone;
+		this.email = email;
+	}
+
+	//method
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public char getGender() {
+		return gender;
+	}
+
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+}
---
+++ myproject02/src/com/oono/p2/ui/CustomerView.java
@@ -0,0 +1,11 @@
+package com.oono.p2.ui;
+/**
+ * 
+ * @description CustomerView为主模块，负责菜单的显示和处理用户操作
+ * @author oono
+ * @version
+ * @date 2020年6月10日下午4:49:26
+ */
+public class CustomerView {
+
+}
